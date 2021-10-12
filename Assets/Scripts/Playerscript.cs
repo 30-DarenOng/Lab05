2ff8f713894a71822d68700581d9e161292996c7
30-DarenOng <91870563+30-DarenOng@users.noreply.github.com>
2021-10-12 20:47:31 +0800

    Added sound, added particles, SUPRAPHYSIOLOGY

--- Assets/Scripts/Playerscript.cs
+++ Assets/Scripts/Playerscript.cs
@@ -8,31 +8,74 @@ public class Playerscript : MonoBehaviour
 {
     private int score;
     public Text ScoreText;
-    bool gameOver;
+   
+    public Text TimerText;
+
+    public float TimeLeft;
+    public float timeRemaining;
+    public float timeValue;
+
+    public ParticleSystem whey;
+    public AudioClip Supraphysiological;
+    public AudioSource SAUCED;
+
     // Start is called before the first frame update
     void Start()
     {
         ScoreText.text = ("Score: " + score);
+        SAUCED = GetComponent<AudioSource>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Countdowntimer();
+        if (score == 100 && TimeLeft <= timeValue)
+        {
+            SceneManager.LoadScene("GameWinScene");
+            Cursor.lockState = CursorLockMode.None;
+        }
+        if (TimeLeft <= 0)
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }    
+
     }
+    private void Countdowntimer()
+    {
+       
+        TimeLeft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(TimeLeft % 60);
+        TimerText.text = "Time Left: " + timeRemaining.ToString();
+
+    }
+
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Coin")
         {
             score += 10 ;
             ScoreText.text = ("Score: " + score);
+            Instantiate(whey, collider.transform.position, Quaternion.identity);
             Destroy(collider.gameObject);
+
+            SAUCED.PlayOneShot(Supraphysiological);
+
         }
         if (collider.gameObject.layer == 4)
         {
-            SceneManager.LoadScene("GameLoseScene");
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            gameOver();
         }
 
     }
+    void gameOver()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene("GameLoseScene");
+    }    
    
 }
