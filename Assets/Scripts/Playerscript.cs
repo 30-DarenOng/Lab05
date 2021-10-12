dd80ef74e82c386ec98eacbe892a67fa00bf5f8e
30-DarenOng <91870563+30-DarenOng@users.noreply.github.com>
2021-10-12 19:33:22 +0800

    Done step 5. Created collision for coin and water and ui for score

---
+++ Assets/Scripts/Playerscript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Playerscript : MonoBehaviour
+{
+    private int score;
+    public Text ScoreText;
+    bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText.text = ("Score: " + score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Coin")
+        {
+            score += 10 ;
+            ScoreText.text = ("Score: " + score);
+            Destroy(collider.gameObject);
+        }
+        if (collider.gameObject.layer == 4)
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
+
+    }
+   
+}
